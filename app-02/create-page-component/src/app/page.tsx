735183c0dd0e844fdeb65d9d59fdefe6baa974de
Your Name <arafathmd432@gmail.com>
2025-11-14 23:30:17 +0600

    fist page creation in next done

---
+++ app-02/create-page-component/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function home() {
+  return (
+    <h1 style={{color:"aqua",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"center"
+    }}>Welcome into the home page!</h1>
+  )
+}
