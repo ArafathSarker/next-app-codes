9dc79a0e4ff431b99d0c1e715dbbd79803753c4d
arafath <arafathmd432@gmail.com>
2025-11-15 11:42:25 +0600

    basic routing creation done

---
+++ app-03/basic-routing-with-file-sytem/src/app/user/page.tsx
@@ -0,0 +1,12 @@
+export default function page() {
+  return (
+   <h1
+   style={{
+    color:"blue",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+   }}
+   >Hello I am user route page.</h1>
+  )
+}
