34796763d41702f1d83aa0d4931a35f3972afd53
Your Name <arafathmd432@gmail.com>
2025-11-20 02:21:36 +0600

    app-13 done

---
+++ app-13/secrate-file-with-no-route/src/app/_secret/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+        <h1>This is not available in route.</h1>
+    </div>
+  )
+}
---
+++ app-13/secrate-file-with-no-route/src/app/user/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+        <h1>Hello this is user page.</h1>
+    </div>
+  )
+}
