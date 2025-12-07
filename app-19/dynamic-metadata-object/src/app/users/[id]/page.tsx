d03970e9e413b8c30dc467a579011c13ff97fbc8
Your Name <arafathmd432@gmail.com>
2025-12-07 13:44:09 -0500

    dynamic metadata function generateMetadata()

---
+++ app-19/dynamic-metadata-object/src/app/users/[id]/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import { resolve } from "path";
+export const generateMetadata = async ({params}:
+    {params:Promise<{id:string}>}): Promise<Metadata> =>{
+    const {id} = await params;
+    return{
+        title:`Product id ${id}`
+    };
+}
+export default async function page({params}:{params:Promise<{id:string}>}) {
+    const {id} = await params;
+    return (
+    <div>
+        <h1>The id of the product is:{id}</h1>
+        </div>
+  )
+}
