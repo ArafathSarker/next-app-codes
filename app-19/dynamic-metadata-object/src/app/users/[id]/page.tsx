9eed23b80f1a23fa8e25778ab620825220c3bf3f
Your Name <arafathmd432@gmail.com>
2025-12-08 12:03:27 +0600

    title object under metadata ={ title: , template:,absolute:}

--- app-19/dynamic-metadata-object/src/app/users/[id]/page.tsx
+++ app-19/dynamic-metadata-object/src/app/users/[id]/page.tsx
@@ -7,6 +7,7 @@ export const generateMetadata = async ({params}:
         title:`Product id ${id}`
     };
 }
+
 export default async function page({params}:{params:Promise<{id:string}>}) {
     const {id} = await params;
     return (
---
+++ app-20/metadata-title-object/src/app/users/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next"
+export const metadata:Metadata ={
+   title:"hola"
+} ;
+export default function page() {
+  return (
+    <div>
+        <h1>This is users page.</h1>
+    </div>
+  )
+}
