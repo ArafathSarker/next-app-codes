fbf13514dd6a520ff7fb4b8c7001ce67f5379446
MD Arafath Sarker <arafathmd432@gmail.com>
2026-01-12 08:52:30 +0000

    app-44 middleware 1

--- app-44/middleware/src/middleware.ts
+++ app-44/middleware/src/middleware.ts
@@ -2,9 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request:NextRequest){
     // return NextResponse.redirect(new URL("/",request.url));
-    if(request.nextUrl.pathname === "/test"){
-       return NextResponse.redirect(new URL("/",request.nextUrl));
+    // if(request.nextUrl.pathname === "/test"){
+    //    return NextResponse.redirect(new URL("/",request.nextUrl));
+    // }
+    const response = NextResponse.next();
+    const theme = request.cookies.get("theme");
+    if(!theme)
+    {
+        response.cookies.set("theme","dark");
+        
     }
+    response.headers.set("custom-header","custom-value");
+    return response;
 }
 
 // export const config = {
